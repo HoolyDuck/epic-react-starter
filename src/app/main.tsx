a92843532d7c0f3b237679e2e04350be4a9384de
Nazarii <hoolyworkacc@gmail.com>
2024-02-24 21:27:52 +0200

    router

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { Provider } from 'react-redux';
 import { store } from '../lib/store/store.ts';
+import { RouterProvider } from '@/lib/router/router-provider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <RouterProvider
+                routes={[
+                    {
+                        path: '/',
+                        element: <App />,
+                        children: [
+                            // ... other routes
+                        ],
+                    },
+                ]}
+            />
         </Provider>
     </React.StrictMode>,
 );
